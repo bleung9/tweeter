debc6b7feb23208d4c7fa0524a8d1bff1ea83817
Bosco <bleung9@gmail.com>
2019-03-20 18:25:29 +0000

    added functionality to display tweets from a 'database'

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,48 +4,93 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
-
-function createTweetElement(data) {
-  let img = data.user.avatars.small;
-  let handle = data.user.handle;
-  let tweet = data.content.text;
-  let time = data.created_at;
-  let name = data.user.name;
-  let $new_tweet = $("<article>").addClass("tweet");
-  let template = `
-          <header>
-            <img src="${img}" />
-            <h2>${name}</h2>
-            <span class="user-handle">${handle}</span>
-          </header>
-          <div class="body">${tweet}</div>
-          <footer>${time}</footer>`
-  $($new_tweet).append(template);
-  return $new_tweet;
-}
-
-
 $(document).ready(function() {
-  var $tweet = createTweetElement(tweetData);
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = data = [
+    {
+      "user": {
+          "name": "Pikachu",
+          "avatars": {
+            "small":   "/images/2rIMflI.png",
+            "regular": "/images/2rIMflI.png",
+            "large":   "/images/2rIMflI.png"
+          },
+          "handle": "@ashs_pikachu"
+        },
+        "content": {
+          "text": "Pika pika!"
+        },
+        "created_at": 1461116232227
+      },
+      {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
 
+  function createTweetElement(data) {
+    let img = data.user.avatars.small;
+    let handle = data.user.handle;
+    let tweet = data.content.text;
+    let time = data.created_at;
+    let name = data.user.name;
+    let $new_tweet = $("<article>").addClass("tweet");
+    let template = `
+            <header>
+              <img src="${img}" />
+              <h2>${name}</h2>
+              <span class="user-handle">${handle}</span>
+            </header>
+            <div class="body">${tweet}</div>
+            <footer>${time}</footer>`
+    $($new_tweet).append(template);
+    return $new_tweet;
+  }
 
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  function renderTweets(tweets) {
+    for (i = 0; i < tweets.length; i++) {
+      $('#tweets-container').append(createTweetElement(tweets[i]));
+    }
+  }
+  renderTweets(data);
 });
