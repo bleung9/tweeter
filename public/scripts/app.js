be2ec9eb8dc33caf7788620f86b1f1dcfba6323f
Bosco <bleung9@gmail.com>
2019-03-21 13:20:27 +0000

    added display error messages if tweet too long or empty tweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,6 +6,12 @@
 
 $(document).ready(function() {
 
+  function escape(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
   function createTweetElement(data) {
     let tweet = data.content.text;
     let img = data.user.avatars.small;
@@ -15,41 +21,47 @@ $(document).ready(function() {
     let $new_tweet = $("<article>").addClass("tweet");
     let template = `
             <header>
-              <img src="${img}" />
-              <h2>${name}</h2>
-              <span class="user-handle">${handle}</span>
+              <img src="${escape(img)}" />
+              <h2>${escape(name)}</h2>
+              <span class="user-handle">${escape(handle)}</span>
             </header>
-            <div class="body">${tweet}</div>
-            <footer>${time}</footer>`
+            <div class="body">${escape(tweet)}</div>
+            <footer>${escape(time)}</footer>`
     $($new_tweet).append(template);
     return $new_tweet;
   }
 
   function renderTweets(data) {
+    $("#tweets-container").empty();
     for (i = 0; i < data.length; i++) {
       $('#tweets-container').prepend(createTweetElement(data[i]));
     }
   }
 
   function loadTweets() {
-    $.get("/tweets", function(data) {
+    return $.get("/tweets", function(data) {
       renderTweets(data);
     });
   }
 
   $("#new-form").submit(function(event) {
     event.preventDefault();
+    $(".new-tweet .counter").text("140");
     let tweet = $(this).serialize();
     let tweet_length = $(this).find("textarea").val().length;
     if (tweet_length === 0) {
-      alert("YOU DIDN'T ENTER ANYTHING (though I didn't check to see if you entered a bunch of newlines ;)");
+      $(".new-tweet .error-msg").text("YOU DIDN'T ENTER ANYTHING, ARE YOU BLIND?!!");
       return;
     }
     else if (tweet_length > 140) {
-      alert("YOUR TWEET IS TOO LONG!!!!!!!")
+      $(".new-tweet .error-msg").text("THE COUNTER IS THERE, ARE YOU BLIND?!!!");
       return;
     }
-    $.post("/tweets", tweet);
+    $(".new-tweet .error-msg").text("");
+    $.post("/tweets", tweet).then(() => {
+      loadTweets();
+      $(this).find("textarea").val("");
+    });
   });
 
   loadTweets();
