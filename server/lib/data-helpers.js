ab2b3a9015d0322652d7f5d1cd435ea5ebffb625
Bosco <bleung9@gmail.com>
2019-03-22 16:39:30 +0000

    finished setting up tweeter to retrieve and save tweets to mongoDB

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -3,24 +3,25 @@
 // Simulates the kind of delay we see with network or filesystem operations
 const simulateDelay = require("./util/simulate-delay");
 
-// Defines helper functions for saving and getting tweets, using the database `db`
+// Defines helper functions for saving and getting tweets, using the mongo database `db`
 module.exports = function makeDataHelpers(db) {
   return {
 
-    // Saves a tweet to `db`
+    // Saves a tweet to `db` (a mongo db)
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+        db.collection("tweets").insertOne(newTweet, (err, results) => {
+          if (err) throw err;
+          callback(null, true);
+        });
     },
 
-    // Get all tweets in `db`, sorted by newest first
+    // Get all tweets in `db` (a mongo db), sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+        db.collection("tweets").find().toArray((err, results) => {
+          if (err) throw err;
+          const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+          callback(null, results.sort(sortNewestFirst));
+        });
     }
 
   };
